32407cc93766658acd61a2169e92d97fd405e679
GMarcati <gonza.marcati@gmail.com>
2020-05-05 18:07:23 -0300

    Varias modificaciones realizadas

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,9 @@
-// JavaScript source code
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("Estoy esperando una dirección de correo electrónico!");
+    } else {
+        email.setCustomValidity("");
+    }
+});
