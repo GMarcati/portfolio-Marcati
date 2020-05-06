1367d06c69a1aa13931699d63a020f41b496a6ad
GMarcati <gonza.marcati@gmail.com>
2020-05-06 12:15:13 -0300

    Modificaciones 06/05

--- js/funciones.js
+++ js/funciones.js
@@ -1,9 +1,23 @@
-const email = document.getElementById("mail");
 
-email.addEventListener("input", function (event) {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("Estoy esperando una dirección de correo electrónico!");
-    } else {
-        email.setCustomValidity("");
-    }
-});
+
+
+function validarMail() {
+
+    const email = document.getElementById("mailForm");
+    email.addEventListener("input", function (event) {
+
+        if (email.validity.typeMismatch) {
+            email.setCustomValidity("Estoy esperando una dirección de correo electrónico!");
+        } else {
+            email.setCustomValidity("");
+        }
+    });
+
+}
+
+
+function validar(){
+
+
+
+}
